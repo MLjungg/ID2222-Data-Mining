d12092fa5a3ec314d17821cf6515248e0b5b6c1a
milj <mikael.lj@hotmail.com>
2020-10-28 15:27:10 +0100

    Started working on lab1

--- Lab 1 - Finding Similar Items/main.py
+++ Lab 1 - Finding Similar Items/main.py
@@ -3,8 +3,9 @@ from pyspark.sql import SparkSession
 spark = SparkSession.builder.master('local[*]').appName('MyApp').getOrCreate()
 sc = spark.sparkContext
 
-textFile = sc.textFile('./data/bbc/business/*.txt')
-words = textFile.flatMap(lambda x: x.split(' '))
+textFile = sc.textFile('./data/bbc/business/001.txt')
+characters = textFile.flatMap(lambda x: "".join(x)).collect()
+words = textFile.flatMap(lambda x: x.split(" "))
 ones = words.map(lambda x: (x, 1))
 counts = ones.reduceByKey(lambda x, y: x+y)
 
