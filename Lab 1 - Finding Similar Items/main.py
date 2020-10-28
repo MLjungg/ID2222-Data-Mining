1adc597bdb80a3c9b27d42a36f31f3c32c896bc1
viktori <viktori@kth.se>
2020-10-28 17:22:03 +0100

    minor changes

--- Lab 1 - Finding Similar Items/main.py
+++ Lab 1 - Finding Similar Items/main.py
@@ -3,9 +3,19 @@ from pyspark.sql import SparkSession
 spark = SparkSession.builder.master('local[*]').appName('MyApp').getOrCreate()
 sc = spark.sparkContext
 
-textFile = sc.textFile('./data/bbc/business/001.txt')
-characters = textFile.flatMap(lambda x: "".join(x)).collect()
-words = textFile.flatMap(lambda x: x.split(" "))
+def create_shingles(data, k):
+    characters = data.flatMap(lambda x: "".join(x))
+    n_grams = characters.flatMap(lambda x: zip(x, x[1:]))
+    return n_grams
+
+def load_data(path):
+    textFiles = sc.textFile(path)
+    return textFiles
+
+textFiles = load_data('./data/bbc/business/001.txt')
+create_shingles(textFiles, 3)
+
+words = textFiles.flatMap(lambda x: x.split(" "))
 ones = words.map(lambda x: (x, 1))
 counts = ones.reduceByKey(lambda x, y: x+y)
 
