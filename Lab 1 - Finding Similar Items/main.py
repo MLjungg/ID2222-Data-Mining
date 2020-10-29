4a22590a082fd5030382fb1542f1567c380cc674
milj <mikael.lj@hotmail.com>
2020-10-29 11:04:49 +0100

    Added Ngram column.

--- Lab 1 - Finding Similar Items/main.py
+++ Lab 1 - Finding Similar Items/main.py
@@ -1,22 +1,39 @@
 from pyspark.sql import SparkSession
+import os
+from pyspark.sql import Row
+from pyspark.sql import functions as F
+from pyspark.ml.feature import NGram, CountVectorizer, VectorAssembler
 
-spark = SparkSession.builder.master('local[*]').appName('MyApp').getOrCreate()
-sc = spark.sparkContext
 
-def create_shingles(data, k):
-    characters = data.flatMap(lambda x: "".join(x))
-    n_grams = characters.flatMap(lambda x: zip(x, x[1:]))
-    return n_grams
+def setup_spark():
+    spark = SparkSession.builder.master('local[*]').appName('MyApp').getOrCreate()
+    sc = spark.sparkContext
+    return spark, sc
 
-def load_data(path):
-    textFiles = sc.textFile(path)
-    return textFiles
 
-textFiles = load_data('./data/bbc/business/001.txt')
-create_shingles(textFiles, 3)
+def create_shingles(data_frames, k):
+    new_data_frames = []
+    ngram = NGram(n=k, inputCol="Chars", outputCol=k + "grams")
 
-words = textFiles.flatMap(lambda x: x.split(" "))
-ones = words.map(lambda x: (x, 1))
-counts = ones.reduceByKey(lambda x, y: x+y)
+    for data_frame in data_frames:
+        new_data_frame = data_frame.withColumn("Chars", F.split(data_frame.Text, ""))
+        new_data_frame = ngram.transform(new_data_frame)
+        new_data_frames.append(new_data_frame)
 
-print(counts.collect())
+    return new_data_frames
+
+def load_data():
+    data_frames = []
+
+    for _, dirs, _ in os.walk("./data/bbc"):
+        for dir in dirs:
+            data_frame = sc.wholeTextFiles("./data/bbc/" + dir + "/*.txt").toDF()
+            data_frame = data_frame.selectExpr("_1 as Filename", "_2 as Text")
+            data_frames.append(data_frame)
+
+    return data_frames
+
+
+spark, sc = setup_spark()
+data_frames = load_data()
+create_shingles(data_frames, 3)
