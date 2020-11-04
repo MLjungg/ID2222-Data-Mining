c8872c52a1f0ebabbed6a257585ca6389f380ec4
milj <mikael.lj@hotmail.com>
2020-11-04 11:48:36 +0100

    Comparing sets works

--- Lab 1 - Finding Similar Items/main.py
+++ Lab 1 - Finding Similar Items/main.py
@@ -1,9 +1,10 @@
 from pyspark.sql import SparkSession
 import os
 from pyspark.sql import functions as F
-from pyspark.sql.functions import when, lit
+from pyspark.sql.functions import rand, when, lit
 from pyspark.ml.feature import NGram
-import pandas
+import itertools
+import math
 
 
 def setup_spark():
@@ -18,7 +19,10 @@ def create_characteristic_matrix(characteristic_matrix):
                                                                  when(characteristic_matrix[document].isNull(),
                                                                       0).otherwise(1))
 
-    return characteristic_matrix.drop('shingles')
+    # Set a random number in range {0, 2^32-1} to each shingle
+    characteristic_matrix = characteristic_matrix.withColumn('shingles', rand() * ((2**32) - 1))
+
+    return characteristic_matrix
 
 
 def create_shingles(data_frames, k):
@@ -39,6 +43,7 @@ def create_shingles(data_frames, k):
         shingle_frame = new_data_frame.select(F.explode(new_data_frame.Ngrams)).distinct()
         shingle_frames.append(shingle_frame)
 
+
     # TODO: Check if all_ngrams grows each iteration
     for i in range(1, len(shingle_frames)):
         all_shingles = shingle_frames[0].join(shingle_frames[i], on=["col"], how="outer")
@@ -49,6 +54,7 @@ def create_shingles(data_frames, k):
     for index, ngram_frame in enumerate(shingle_frames):
         all_shingles = all_shingles.join(ngram_frame, all_shingles.shingles == ngram_frame.col, how="left")
         all_shingles = all_shingles.withColumnRenamed("col", document_types[index])
+
 
     return all_shingles
 
@@ -68,13 +74,26 @@ def load_data():
     return data_frames, document_types
 
 
-def min_hashing(characteristic_matrix):
-    size = characteristic_matrix
+def compare_sets(shingles):
+    pairwise_document_combinations = list(itertools.combinations(document_types, 2))
+
+    for document1, document2 in pairwise_document_combinations:
+        shingles = shingles.withColumn(document1 + "&" + document2 + " union", shingles[document1].bitwiseOR(shingles[document2]))
+        shingles = shingles.withColumn(document1 + "&" + document2 + " intersect", shingles[document1].bitwiseAND(shingles[document2]))
+
+        union = shingles.select(F.sum(shingles[document1 + "&" + document2 + " union"])).collect()[0][0]
+        intersect = shingles.select(F.sum(shingles[document1 + "&" + document2 + " intersect"])).collect()[0][0]
+
+        print("Jaccard similarity between " + document1 + " and " + document2 + " is " + str(round(intersect/union, 2)))
 
 
 spark, sc = setup_spark()
 data_frames, document_types = load_data()
-all_shingles = create_shingles(data_frames, 3)
-characteristic_matrix = create_characteristic_matrix(all_shingles)
-
+shingles = create_shingles(data_frames, 5)
+characteristic_matrix = create_characteristic_matrix(shingles)
+compare_sets(characteristic_matrix)
 signature_matrix = min_hashing(characteristic_matrix)
+
+# TODO: Lowercase all words?
+
+
