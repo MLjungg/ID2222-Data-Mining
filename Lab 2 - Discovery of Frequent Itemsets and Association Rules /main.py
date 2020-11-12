9cf9232e6445f6b114d4d493c234be9a59b01c7f
milj <mikael.lj@hotmail.com>
2020-11-12 17:12:30 +0100

    Refactored

--- Lab 2 - Discovery of Frequent Itemsets and Association Rules /main.py
+++ Lab 2 - Discovery of Frequent Itemsets and Association Rules /main.py
@@ -1,11 +1,12 @@
 import itertools
 
-def a_priori_algorithm(baskets, similarity_threshold):
-    count = {}
-    frequent_items = []
-    frequent_items = {}
 
-    # first pass (n) --> lägg till
+def a_priori_algorithm(baskets):
+    # Init variables
+    count = {}
+    similarity_threshold = 500
+
+    # first pass (n=1)
     for basket in baskets:
         for item in basket:
             item = tuple([item])
@@ -14,18 +15,18 @@ def a_priori_algorithm(baskets, similarity_threshold):
             else:
                 count[item] += 1
 
-    # second pass
+    # second pass (n --> infinity)
     stored_itemsets = n_pass(baskets, similarity_threshold, count)
-
 
     return stored_itemsets
 
+
 def n_pass(baskets, similarity_threshold, count):
     n = 1
-    new_itemsets = True
+    new_itemsets = True    # Stores information about if we shall look for a higher order of itemset.
     stored_itemsets = {}
     while new_itemsets:
-        n = n+1
+        n = n + 1
         stored_itemsets[n] = set()
         new_itemsets = False
         for index, basket in enumerate(baskets):
@@ -48,39 +49,57 @@ def n_pass(baskets, similarity_threshold, count):
                 else:
                     count[itemset] += 1
 
+    return stored_itemsets
+
+
 def generate_itemsets(frequent_itemset, count, n, similarity_threshold):
     # This function generates valid n+1 itemsets given a n-itemset.
     # E.g input [1,2] [2,3] [3,4] --> Which triples can we make?
-    # Each number has to appear n-1 times to be in the itemset. E.g for the triple [1,2,3] to be valid the following pairs most exist: [1,2], [2,3], [1,3]
 
     valid_itemsets = []
-    if n == 2:  # Trivial when creating pairs (all pairs are valid)
+    if n == 2:  # Trivial case, all pairs are valid.
         return list(itertools.combinations(frequent_itemset, 2))
     else:
         itemsets = itertools.combinations(frequent_itemset, n)
         for itemset in itemsets:
-            valid_itemset = True
-            # First check if respective tuple is frequent
-            for items in itemset:
-               if count[items] < similarity_threshold:
-                    valid_itemset = False
+            valid_itemset = check_frequent(itemset, similarity_threshold, count)
             if valid_itemset:
-                # Now check if the remaining tuples can create a itemset of a higher order
-                singletons = []
-                for items in itemset:
-                    for singleton in items:
-                        singletons.append(singleton)
-                set_singletons = set(singletons)
-                for singleton in set_singletons:
-                    if singletons.count(singleton) != n-1:  # E.g for the triple [1,2,3] to be valid each singleton needs to exist 3-1 times.
-                        valid_itemset = False
+                valid_itemset, set_singletons = higher_order_validation(itemset, n) # set_singletons return the n+1-itemset.
 
+                # If still valid, store the new itemset
                 if valid_itemset:
                     valid_itemsets.append(tuple(sorted(set_singletons)))
 
         return valid_itemsets
 
 
+def check_frequent(itemset, similarity_threshold, count):
+    #  This fuctions checks if respective itemset that constitutes the new itemset is frequent --> eg: if we have [1,2,3] check if [1,2] [2,3] [1,3] is frequent.
+    valid_itemset = True
+    for items in itemset:
+        if count[items] < similarity_threshold:
+            valid_itemset = False
+
+    return valid_itemset
+
+
+def higher_order_validation(itemset, n):
+    # This function check if the new itemset can be constructed from the current itemset --> eg. to create the triple [1,2,3] the following pairs most exist: [1,2], [2,3], [1,3]
+    valid_itemset = True
+    singletons = []
+    for items in itemset:
+        for singleton in items:
+            singletons.append(singleton)
+    set_singletons = set(singletons)
+    for singleton in set_singletons:
+        if singletons.count(singleton) != n - 1:  # E.g for the triple [1,2,3] to be valid each singleton needs to exist 3-1 times.
+            valid_itemset = False
+            break
+
+    if valid_itemset:
+        return valid_itemset, set_singletons
+    else:
+        return valid_itemset, []
 
 
 def load_data():
@@ -93,9 +112,8 @@ def load_data():
 
 
 def main():
-    similarity_threshold = 500
     baskets = load_data()
-    frequent_itemsets = a_priori_algorithm(baskets, similarity_threshold) # This return all
+    frequent_itemsets = a_priori_algorithm(baskets)  # This return all
     print("Done")
 
 
